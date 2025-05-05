da540b140f627c387f16117f1db3ef04a482e178
jhons <jho951@naver.com>
2025-05-05 15:22:45 +0900

    fix: @

--- backend/src/main/java/com/dajava/backend/BackendApplication.java
+++ backend/src/main/java/com/dajava/backend/BackendApplication.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @EnableScheduling
 @SpringBootApplication
 @ServletComponentScan
-@EnableJpaAuditing
 @EnableAspectJAutoProxy
 public class BackendApplication {
 
